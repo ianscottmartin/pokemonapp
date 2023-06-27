084b3a908835ee17fd8bb6336c95e7e3f70dc6c4
Ian Scott Martin <ianscottmartin@gmail.com>
2023-06-27 17:07:47 -0600

    setupstyle

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import { React } from 'react';
 import Main from './Components/Main';
+import './Components/style.css'
 
 import './App.css';
 
 function App() {
   return (
     <>
-      <Main />
+      <Main/>
 
     </>
   );
