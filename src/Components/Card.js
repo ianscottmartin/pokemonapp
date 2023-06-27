b13ba9ba97bed668ecb1fa249db9d823f6d1747d
Ian Scott Martin <ianscottmartin@gmail.com>
2023-06-27 17:33:18 -0600

    setupfirst image

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -3,7 +3,10 @@ import React from "react";
 const Card = () => {
     return (
         <>
-
+            <div classNAme="card">
+            <h2>1</h2>
+            <img src="" />
+            </div>
         </>
     )
 }
