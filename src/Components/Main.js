b9de2a217347a7d4856802676586b2fac6c8dedd
Ian Scott Martin <ianscottmartin@gmail.com>
2023-06-27 18:44:04 -0600

    useeffect

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,14 +1,19 @@
 import React, { useState } from "react";
 import Card from "./Card";
 import PokemonInfo from "./PokemonInfo";
-
+import { useEffect } from "react";
 
 import axios from "axios"
 
 const Main = () => {
     const [pokeData, setPokeData] = useState([]);
     const [loading, setLoading] = useState(true);
-    const [url, setUrl] = useState()
+    const [url, setUrl] = useState("https://pokeapi.co/api/v2/pokemon/")
+
+    useEffect(() => {
+
+    })
+
     return (
         <>
             <div className="container">
